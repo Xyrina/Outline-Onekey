53f2cc37fbc8ce479d240d0c28c65eada6bc26fa
Xyrina <58162650+Xyrina@users.noreply.github.com>
2019-11-25 17:38:52 +0800

    Update thrive.sh

--- thrive.sh
+++ thrive.sh
@@ -54,16 +54,11 @@ echo -e "${Info} 安装完成"
 Find(){
 sudo cat /opt/outline/access.txt
 echo -e "${Info} 
-
-apiURL范例"
-
-echo -e "${Info} 
+apiURL范例
 { 
   "apiUrl": "https://1.2.3.4:1234/XXXXXXXXXXXX", 
   "certSha256": "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" 
 }"
-
-}
 
 Restart(){
 sudo service docker start
