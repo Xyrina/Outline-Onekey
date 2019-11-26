14be0ad57f475b07ddb87e9ac7fe27133143f017
Xyrina <58162650+Xyrina@users.noreply.github.com>
2019-11-26 15:34:37 +0800

    Update thrive.sh

--- thrive.sh
+++ thrive.sh
@@ -7,7 +7,7 @@ sh_ver="1.0.4"
 
 
 # Font set
-Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
+Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" &&  Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
 Tip="${Green_font_prefix}[注意]${Font_color_suffix}"
@@ -59,7 +59,7 @@ sudo cat /opt/outline/access.txt
 
 Restart(){
 sudo systemctl restart docker.service
-echo -e "${Info} 重启完毕"
+echo -e "${Tip} 重启完毕"
 }
 
 echo && echo -e "  Outline一键脚本 ${Red_font_prefix}[v${sh_ver}]${Font_color_suffix}
@@ -73,7 +73,7 @@ by Isaac 20191125
  ${Green_font_prefix} 3.${Font_color_suffix} 重启Outline
 ————————————"&& echo
 
-	read -e -p "请输入数字 [1-3]:" num
+	read -e -p -r "请输入数字 [1-3]:" num
 	case "$num" in
 		1)
 		Install
