65b9d7e4f67e0491b00b129e7e7792edf95e9120
Xyrina <58162650+Xyrina@users.noreply.github.com>
2019-11-26 16:23:44 +0800

    Update thrive.sh

--- thrive.sh
+++ thrive.sh
@@ -73,7 +73,8 @@ echo && echo -e "  Outline一键脚本 ${Red_font_prefix}[v${sh_ver}]${Font_colo
  ${Green_font_prefix} 3.${Font_color_suffix} 重启Outline
 ————————————"
 
-read -p -r " 请输入数字 [1-3]:" num
+printif " 请输入数字 [1-3]:"
+read -r num
 case $num in
 	1)
 	Install
