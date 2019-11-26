3bf29a5965857a61eb11dd6192515529325cc476
Xyrina <58162650+Xyrina@users.noreply.github.com>
2019-11-26 16:44:54 +0800

    Update thrive.sh

--- thrive.sh
+++ thrive.sh
@@ -71,10 +71,11 @@ echo && echo -e "  Outline一键脚本 ${Red_font_prefix}[v${sh_ver}]${Font_colo
  ${Green_font_prefix} 2.${Font_color_suffix} API信息
  ————————————
  ${Green_font_prefix} 3.${Font_color_suffix} 重启Outline
-————————————"
-echo 
-read -p "输入 1 到 3 之间的数字:" num
-case $num in
+———————————— "
+echo
+
+read -p "输入 1 到 3 之间的数字:" input
+case $input in
 	1)
 	Install
 	;;
